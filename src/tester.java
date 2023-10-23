b44b54ccdb1f82cf4628ffa2c971bc85b6a23dcd
Yunusov Jamoliddin <legion26042010@gmail.com>
2023-10-23 12:11:05 +0300

    Test

--- src/tester.java
+++ src/tester.java
@@ -1,6 +1,32 @@
+import java.util.Scanner;
+
 class tester {
-    private int width;
-    double heigth;
-    double depth;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+
+        for (int i = 0; i < t; i++) {
+            String s = scanner.next();
+            int n = s.length();
+
+            int start = 0;
+            int end = 0;
+            String longestSubstring = "";
+
+            while (end < n) {
+                if (s.substring(start, end + 1).equals(s.substring(end))) {
+                    longestSubstring = s.substring(start, end + 1);
+                } else {
+                    start++;
+                }
+
+                end++;
+            }
+
+            System.out.println(longestSubstring);
+        }
+
+    }
+
 
 }
