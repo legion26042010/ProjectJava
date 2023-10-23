d7fa18a6daa3d8d80fc683a0d4f9962efb4b3151
Yunusov Jamoliddin <legion26042010@gmail.com>
2023-10-23 12:56:55 +0300

    Test

--- src/tester.java
+++ src/tester.java
@@ -1,32 +1,32 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 class tester {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int t = scanner.nextInt();
+//        Scanner scanner = new Scanner(System.in);
+//        int t = scanner.nextInt();
+//
+//        for (int i = 0; i < t; i++) {
+//            int n=scanner.nextInt();
+//            int k=scanner.nextInt();
+//            int[] arr=new int[n];
+//            for (int j=0;j<arr.length;j++){
+//                arr[i]=scanner.nextInt();
+//            }
+//            Arrays.sort(arr);
+//            int d=0, max=0;
 
-        for (int i = 0; i < t; i++) {
-            String s = scanner.next();
-            int n = s.length();
-
-            int start = 0;
-            int end = 0;
-            String longestSubstring = "";
-
-            while (end < n) {
-                if (s.substring(start, end + 1).equals(s.substring(end))) {
-                    longestSubstring = s.substring(start, end + 1);
-                } else {
-                    start++;
-                }
-
-                end++;
-            }
-
-            System.out.println(longestSubstring);
         }
 
+    public String zipZap(String str) {
+        String s="";
+        for (int i=0;i<str.length()-2;i++){
+            if (str.substring(i,i+1).equals("z") && str.substring(i+2,i+3).equals("p")) {
+                s+=str.substring(i,i+1)+str.substring(i+2,i+3);
+            }else {
+                s+=str.substring(i,i+1);
+            }
+        }
+        return s;
     }
-
-
 }
