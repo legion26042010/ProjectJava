a7926315a46fe7e8384f84a561c0b108e340a000
Yunusov Jamoliddin <legion26042010@gmail.com>
2023-10-25 08:35:55 +0300

    Test

--- src/tester.java
+++ src/tester.java
@@ -4,16 +4,18 @@ public class tester {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        String[] arr={ "Sheldon", "Leonard", "Penny", "Rajesh", "Howard"};
-        double num=n;
-        try {
-            double number=((num%5)/5);
-            String nums= String.valueOf(number).replace("0","").replace(".","");
-            int res= Integer.parseInt(nums);
-            System.out.println(arr[res-2]);
-        }catch (NumberFormatException formatException){
-            System.out.println(arr[0]);
+        int k=scanner.nextInt();
+        List<Integer> list=new ArrayList<>();
+        while (n-->0){
+            list.add(scanner.nextInt());
         }
-
+        int max=Integer.MIN_VALUE;
+        Collections.sort(list);
+        List<Integer> arr=new ArrayList<>();
+        for (int i=0;i<k;i++){
+           arr.add(list.get(i));
+           max=Math.max(arr.get(i),max);
+        }
+        System.out.println(max+1);
     }
 }
