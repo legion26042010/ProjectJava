859327a5695fd24c6fb03089136b165dbcc98d29
Legion <legion26042010@gmail.com>
2024-01-13 12:09:28 +0300

    repds

--- src/to.java
+++ src/to.java
@@ -1,37 +1,23 @@
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Scanner;
+import java.util.*;
 
 public class to {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int t = scanner.nextInt();
-        while (t-- > 0) {
-            int n = scanner.nextInt();
-            int[] arr = new int[n];
-            HashSet<Integer> set = new HashSet<>();
-            for (int i = 0; i < n; i++) {
-                int a = scanner.nextInt();
-                arr[i] = a;
+        int n = scanner.nextInt();
+        int k = scanner.nextInt();
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(scanner.nextInt());
+        }
+        for (int i = 0; i < n; i++) {
+            int l=list.get(0)-k;
+            if (l<=0) {
+                list.remove(0);
+            }else {
+                list.remove(0);
+                list.add(l);
             }
-            int m = 1;
-            Arrays.sort(arr);
-            for (int i = 0; i < arr.length - 1; i++) {
-                if (arr[i] == arr[i + 1] && arr[i] != 0) {
-                    m++;
-                } else {
-                    set.add(arr[i]);
-                    if (m >= 2) {
-                        set.add(arr[i] * -1);
-                    }
-                    m = 1;
-                }
-            }
-            set.add(arr[arr.length - 1]);
-            if (m >= 2) {
-                set.add(arr[arr.length - 1] * -1);
-            }
-            System.out.println(set.size());
+            System.out.println(list);
         }
     }
 }
